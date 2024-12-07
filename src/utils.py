8417619f2a3ce004d98f476f21583d5deba5e666
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2024-12-07 18:26:29 +1000

    create function read_transactions(filepath) in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,43 @@
 import json
+import os
 
-with open('C:\Users\КГА ПОУ ЛИК\PycharmProjects\My_project\data\operation.json') as f:
-    data = json.load(f)
+def read_transactions(filepath):
+    """Читает JSON-файл и возвращает список словарей с данными о финансовых транзакциях"""
 
-print(data)
+    if not os.path.exists(filepath):
+        print(f"Файл '{filepath}' не найден.")
+        return []
+
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if not isinstance(data, list):
+            print(f"Ошибка: содержимое файла '{filepath}' не является списком.")
+            return []
+
+        return data
+
+    except json.JSONDecodeError:
+        print(f"Ошибка: файл '{filepath}' содержит некорректный JSON.")
+        return []
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return []
+
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Корень проекта
+filepath = os.path.join(base_dir, "data", "operation.json")
+# Получение данных о транзакциях
+transactions = read_transactions(filepath)
+
+# Вывод результата
+if transactions:
+    print("Транзакции успешно загружены:")
+    print(transactions)
+else:
+    print("Файл пустой или содержит некорректные данные.")
+
+print("Текущая рабочая директория:", os.getcwd())
+
+
